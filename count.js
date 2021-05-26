23b778885fdbb9932e17fd6c590b7cfcffbe3221
Michael Clautice <michaelclauticeusa@gmail.com>
2021-05-26 13:10:23 -0400

    Mike modified 2 files Wed.5.26.21.

--- count.js
+++ count.js
@@ -5,8 +5,11 @@
 // count.js is a module for app.js
 // counter var is assigned an anon function
 var counter = function(array){
-    return "+++++++++++++\n+++++++++++++\nThere definately are " + array.length + " elements in this demo array.\n+++++++++++++"
+    return "\n+++++++++++++\n+++++++++++++\nThere definately are " + array.length + " elements in this demo array.\n+++++++++++++\n"
 };
+
+console.log(counter(['A', 'B', 'C']));
+
 // The module.exports or exports is a special object which is included in every JS file in the Node.js application by default. Module is a var that represents current module and exports is an object that will be exposed as a module. So, whatever you assign to module.exports or exports, will be exposed as a module.
 module.exports = counter;
 
